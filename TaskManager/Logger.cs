cf6a9153ce9c74b724081bae2dd2908aed21fa61
M1Rodem <Starodvoriovkir@yandex.ru>
2026-03-02 10:48:28 +0300

    added new logger sturcture

--- TaskManager/Logger.cs
+++ TaskManager/Logger.cs
@@ -1,7 +1,9 @@
 ﻿using Serilog;
+using Serilog.Events;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 
 namespace TaskManager
 {
@@ -9,13 +11,42 @@ namespace TaskManager
     {
         public Logger()
         {
+            string dateTimeFolder = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            string logDirectory = Path.Combine("logs", dateTimeFolder);
+            Directory.CreateDirectory(logDirectory);
+
             Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()                               // уровень логирования: Debug и выше
-            .WriteTo.Console()                                  // пишем логи в консоль
-            .WriteTo.File("logs\\myapp-.log",                   // пишем логи в файлы
-                rollingInterval: RollingInterval.Day,           // ежедневная ротация
-                outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
-            .CreateLogger();
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.Logger(lc => lc
+                    .Filter.ByIncludingOnly(ev => ev.Level == LogEventLevel.Debug)
+                    .WriteTo.File(
+                        path: Path.Combine(logDirectory, "log_DBG.log"),
+                        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] {Message:lj}{NewLine}{Exception}",
+                        encoding: Encoding.UTF8
+                    ))
+                .WriteTo.Logger(lc => lc
+                    .Filter.ByIncludingOnly(ev => ev.Level == LogEventLevel.Information)
+                    .WriteTo.File(
+                        path: Path.Combine(logDirectory, "log_INF.log"),
+                        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] {Message:lj}{NewLine}{Exception}",
+                        encoding: Encoding.UTF8
+                    ))
+                .WriteTo.Logger(lc => lc
+                    .Filter.ByIncludingOnly(ev => ev.Level == LogEventLevel.Warning)
+                    .WriteTo.File(
+                        path: Path.Combine(logDirectory, "log_WRN.log"),
+                        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] {Message:lj}{NewLine}{Exception}",
+                        encoding: Encoding.UTF8
+                    ))
+                .WriteTo.Logger(lc => lc
+                    .Filter.ByIncludingOnly(ev => ev.Level >= LogEventLevel.Error)
+                    .WriteTo.File(
+                        path: Path.Combine(logDirectory, "log_ERR.log"),
+                        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level}] {Message:lj}{NewLine}{Exception}",
+                        encoding: Encoding.UTF8
+                    ))
+                .CreateLogger();
 
             //// 2. Пример использования
             //Log.Debug("Отладочное сообщение (Debug).");
